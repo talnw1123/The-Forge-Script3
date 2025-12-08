a7f3857ac21aa250dc557ff1d0d034c415fb2458
talnw1123 <talnw1123@gmail.com>
2025-12-08 23:15:46 +0700

    first commit

--- Loader.lua
+++ Loader.lua
@@ -19,7 +19,7 @@ repeat task.wait(1) until game:IsLoaded()
 ----------------------------------------------------------------
 local CONFIG = {
     -- 🔗 GitHub Raw URL (เปลี่ยนเป็น URL ของคุณ)
-    GITHUB_BASE_URL = "https://raw.githubusercontent.com/talnw1123/The-Forge-Script2/refs/heads/main/",
+    GITHUB_BASE_URL = "https://raw.githubusercontent.com/talnw1123/The-Forge-Script3/refs/heads/main/",
     
     -- ⏱️ Timing
     INITIAL_WAIT = 40,          -- รอเริ่มต้น (วินาที)
